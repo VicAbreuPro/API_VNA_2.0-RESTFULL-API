dc2a598e7f6d5bae37673143884da50c56bcab18
Victor Abreu <94565182+VicAbreuPro@users.noreply.github.com>
2022-01-21 18:28:59 +0000

    Update Conn.cs

--- Data/Conn.cs
+++ Data/Conn.cs
@@ -4,11 +4,11 @@ namespace API_VNA_2._0.Data
     public class Conn
     {
         // Variáveis Auxiliares para utilização dos dados de acesso ao servidor
-        private const string servidor = "204.2.63.87";
-        private const string port = "10295";
+        private const string servidor = "example";
+        private const string port = "example";
         private const string bancoDados = "cli_app";
-        private const string usuario = "admin";
-        private const string senha = "hummerh14x4rally";
+        private const string usuario = "example";
+        private const string senha = "example";
 
         static public string strConn = $"server={servidor};port={port};User Id={usuario};database={bancoDados};password={senha}";
     }
