250ffa50be6488e3446c5a82b85c53fc2944609f
a18841ipca <a18841@alunos.ipca.pt>
2022-02-01 01:07:14 +0000

    update4

--- Data/Conn.cs
+++ Data/Conn.cs
@@ -4,11 +4,11 @@ namespace API_VNA_2._0.Data
     public class Conn
     {
         // Variáveis Auxiliares para utilização dos dados de acesso ao servidor
-        private const string servidor = "myvnadb.mysql.database.azure.com";
-        private const string port = "3306";
-        private const string bancoDados = "cli_app";
-        private const string usuario = "jynx";
-        private const string senha = "Hummerh14x4rally";
+        private const string servidor = "EXAMPLE";
+        private const string port = "EXAMPLE";
+        private const string bancoDados = "EXAMPLE";
+        private const string usuario = "EXAMPLE";
+        private const string senha = "EXAMPLE";
 
         static public string strConn = $"server={servidor};port={port};User Id={usuario};database={bancoDados};password={senha}";
     }
